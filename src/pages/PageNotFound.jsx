21207470c84115655a75b59b6fa6b03c0c50ff6b
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2025-04-26 19:55:02 +0100

    App Routing

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from '../components/PageNav';
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <PageNav />
+      <div>Not Found :( </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
